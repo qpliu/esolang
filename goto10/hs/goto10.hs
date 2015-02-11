ed2fb3608cfaeba254f1f592604e94759cf02ffa
Q.P.Liu <qpliu@yahoo.com>
2015-02-11 11:23:30 -0800

    The GOTO 10 programming language.

---
+++ goto10/hs/goto10.hs
@@ -0,0 +1,73 @@
+import Data.Char(chr,isDigit,isSpace)
+import Data.Map(Map)
+import qualified Data.Map as M
+
+parse :: String -> Map Integer [Stmt]
+parse = foldl parseLine (M.fromList [(0,[line0]),(1,[line1])]) . lines
+
+parseLine :: Map Integer [Stmt] -> String -> Map Integer [Stmt]
+parseLine prog line
+  | take 3 lineContents == "REM" = prog
+  | null lineNum = error "SYNTAX ERROR"
+  | take 4 lineContents /= "GOTO" = addStmt (error "SYNTAX ERROR")
+  | null targetLineNum = addStmt (error "SYNTAX ERROR")
+  | not (null trailingGarbage) = addStmt (error "SYNTAX ERROR")
+  | otherwise = addStmt (goto (read targetLineNum))
+  where
+    (lineNum,lineContents) =
+        break (not . isDigit) (filter (not . isSpace) line)
+    (targetLineNum,trailingGarbage) =
+        break (not . isDigit) (drop 4 lineContents)
+    addStmt stmt =
+        M.alter (maybe (Just [stmt]) (Just . (stmt:))) (read lineNum) prog
+
+
+type Stmt = State -> State
+
+data State = State{
+    stProg :: Map Integer [Stmt],
+    stLine0 :: Integer,
+    stLine1 :: Int,
+    stThreads :: [Stmt]
+    }
+
+goto :: Integer -> Stmt
+goto target st@State{stProg = prog, stThreads = threads} =
+    maybe st update (M.lookup target prog)
+  where update stmts = st{stThreads = stmts ++ threads}
+
+line0 :: Stmt
+line0 st@State{stLine0 = n} = st{stLine0 = n + 1}
+
+line1 :: Stmt
+line1 st@State{stLine1 = n} = st{stLine1 = n + 1}
+
+
+run :: State -> [Bool]
+run st@State{stThreads = threads}
+  | null threads = []
+  | otherwise = output n1 ++ run st''
+  where
+    st'@State{stLine0 = n0} =
+        foldl (flip ($)) st{stLine0 = 0, stLine1 = 0, stThreads = []} threads
+    st''@State{stLine1 = n1} | n0 == 0 = st' | otherwise = goto n0 st'
+    output 0 = []
+    output 1 = [False]
+    output n = bits (n-1)
+    bits 0 = []
+    bits n = (n `mod` 2 == 1) : bits (n `div` 2)
+
+toStr :: [Bool] -> String
+toStr b | null b = [] | otherwise = toChr (take 8 b) : toStr (drop 8 b)
+  where toChr b = chr (sum (zipWith select (reverse b) (iterate (2*) 1)))
+        select b n | b = n | otherwise = 0
+
+
+goto10 :: String -> String
+goto10 = (toStr . run . goto 10 . init)
+  where
+    init prog =
+        State{stProg = parse prog, stThreads = [], stLine0 = 0, stLine1 = 0}
+
+main :: IO ()
+main = interact goto10
