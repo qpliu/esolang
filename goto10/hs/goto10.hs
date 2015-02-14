958dd5e5b6835ab3edc8d591802ac49c2aa2a74d
Q.P.Liu <qpliu@yahoo.com>
2015-02-13 23:39:47 -0800

    Rework GOTO 10 interpreter.

--- goto10/hs/goto10.hs
+++ goto10/hs/goto10.hs
@@ -2,69 +2,44 @@ import Data.Char(chr,isDigit,isSpace)
 import Data.Map(Map)
 import qualified Data.Map as M
 
-parse :: String -> Map Integer [Stmt]
-parse = foldl parseLine (M.fromList [(0,[line0]),(1,[line1])]) . lines
+parse :: String -> Map Integer [Integer]
+parse = foldl parseLine (M.fromList [(0,[]),(1,[])]) . lines
 
-parseLine :: Map Integer [Stmt] -> String -> Map Integer [Stmt]
+parseLine :: Map Integer [Integer] -> String -> Map Integer [Integer]
 parseLine prog line
   | take 3 lineContents == "REM" = prog
   | null lineNum = error "SYNTAX ERROR"
-  | take 4 lineContents /= "GOTO" = addStmt (error "SYNTAX ERROR")
-  | null targetLineNum = addStmt (error "SYNTAX ERROR")
-  | not (null trailingGarbage) = addStmt (error "SYNTAX ERROR")
-  | otherwise = addStmt (goto (read targetLineNum))
+  | take 4 lineContents /= "GOTO" = error "SYNTAX ERROR"
+  | null targetLineNum = error "SYNTAX ERROR"
+  | not (null trailingGarbage) = error "SYNTAX ERROR"
+  | otherwise =
+      M.alter (Just . (read targetLineNum:) . maybe [] id) (read lineNum) prog
   where
     (lineNum,lineContents) =
         break (not . isDigit) (filter (not . isSpace) line)
     (targetLineNum,trailingGarbage) =
         break (not . isDigit) (drop 4 lineContents)
-    addStmt stmt = M.alter (Just . (stmt:) . maybe [] id) (read lineNum) prog
 
-
-type Stmt = State -> State
-
-data State = State{
-    stProg :: Map Integer [Stmt],
-    st0 :: Integer,
-    st1 :: Int,
-    stThreads :: [Stmt]
-    }
-
-goto :: Integer -> Stmt
-goto target st@State{stProg = prog, stThreads = threads} =
-    maybe st update (M.lookup target prog)
-  where update stmts = st{stThreads = stmts ++ threads}
-
-line0 :: Stmt
-line0 st@State{st0 = n} = st{st0 = n + 1}
-
-line1 :: Stmt
-line1 st@State{st1 = n} = st{st1 = n + 1}
-
-
-run :: State -> [Bool]
-run st@State{stThreads = threads}
-  | null threads = []
-  | otherwise = output n1 ++ run st''{st0 = 0, st1 = 0}
+run :: Map Integer Integer -> Map Integer [Integer] -> [Bool]
+run st prog
+  | M.fold (+) 0 st == 0 = []
+  | otherwise = output (st'' M.! 1) ++ run st'' prog
   where
-    st'@State{st0 = n0} = foldl (flip ($)) st{stThreads = []} threads
-    st''@State{st1 = n1} | n0 == 0 = st' | otherwise = goto n0 st'
-    output 0 = []
-    output 1 = [False]
-    output n = bits (n-1)
-    bits 0 = []
-    bits n = (n `mod` 2 == 1) : bits (n `div` 2)
+    st' = M.foldrWithKey goto (M.map (const 0) prog) st
+    st'' | st' M.! 0 == 0 = st'
+         | otherwise = M.alter (fmap (1+)) (st' M.! 0) st'
+    output n | n == 0 = [] | n == 1 = [False] | otherwise = bits (n-1)
+    bits n | n == 0 = [] | otherwise = (n `mod` 2 == 1) : bits (n `div` 2)
+    goto from n state = foldl (flip $ M.alter (fmap (+n))) state
+                              (maybe [] id (M.lookup from prog))
 
 toStr :: [Bool] -> String
 toStr b | null b = [] | otherwise = toChr (take 8 b) : toStr (drop 8 b)
   where toChr b = chr (sum (zipWith select (reverse b) (iterate (2*) 1)))
         select b n | b = n | otherwise = 0
 
-
 goto10 :: String -> String
-goto10 = (toStr . run . goto 10 . init)
-  where
-    init prog = State{stProg = parse prog, stThreads = [], st0 = 0, st1 = 0}
+goto10 = toStr . run (M.fromList [(10,1)]) . parse
 
 main :: IO ()
 main = interact goto10
