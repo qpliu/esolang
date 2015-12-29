422d9d3576850c5517dd69ab656884f4fee67972
Q.P.Liu <qpliu@yahoo.com>
2015-12-29 11:25:30 -0800

    Finish implementing blo compiler to LLVM 3.7.
    Successfully compiles hello.blo and cat.blo.

--- blo/go/blo.go
+++ blo/go/blo.go
@@ -6,9 +6,34 @@ import (
 )
 
 func main() {
+	var srcs []string
+	var outflag, outfile string
+	f := func(tokens <-chan Token, outflag, outfile string) error {
+		return interp(tokens)
+	}
+	if len(os.Args) > 2 && os.Args[1] == "-o" {
+		f = compile
+		outflag = "-o"
+		outfile = os.Args[2]
+		srcs = os.Args[3:]
+	} else if len(os.Args) > 2 && os.Args[1] == "-c" {
+		f = compile
+		outflag = "-c"
+		srcs = os.Args[2:]
+	} else if len(os.Args) > 2 && os.Args[1] == "-S" {
+		f = compile
+		outflag = "-S"
+		srcs = os.Args[2:]
+	} else if len(os.Args) > 0 && (os.Args[0] == "compile" || strings.HasSuffix(os.Args[0], "/compile")) {
+		f = compile
+		srcs = os.Args[1:]
+	} else if len(os.Args) > 0 {
+		srcs = os.Args[1:]
+	}
+
 	tokens := make(chan Token)
 	go func() {
-		for _, file := range os.Args[1:] {
+		for _, file := range srcs {
 			if f, err := os.Open(file); err != nil {
 				os.Stderr.WriteString(err.Error() + "\n")
 				os.Exit(1)
@@ -19,12 +44,7 @@ func main() {
 		}
 		close(tokens)
 	}()
-
-	f := interp
-	if os.Args[0] == "compile" || strings.HasSuffix(os.Args[0], "/compile") {
-		f = compile
-	}
-	if err := f(tokens); err != nil {
+	if err := f(tokens, outflag, outfile); err != nil {
 		os.Stderr.WriteString(err.Error() + "\n")
 		os.Exit(1)
 	}
--- blo/go/compile.go
+++ blo/go/compile.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"errors"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"sync"
 )
 
-func compile(tokens <-chan Token) error {
+func compile(tokens <-chan Token, outflag, outfile string) error {
 	var ast *Ast
 	if _ast, err := Parse(tokens); err != nil {
 		return err
@@ -21,6 +23,46 @@ func compile(tokens <-chan Token) error {
 	if mainFunc == nil || len(mainFunc.Params) != 0 {
 		return errors.New("No 'main' function with zero arguments")
 	}
+	if outflag == "-S" {
+		return LLVMCodeGen(ast, os.Stdout)
+	} else if outflag == "-c" {
+		var waitGroup sync.WaitGroup
+		llcmd := exec.Command("llc", "-filetype=asm")
+		out, err := llcmd.StdinPipe()
+		if err != nil {
+			return err
+		}
+		if in, err := llcmd.StdoutPipe(); err != nil {
+			return err
+		} else {
+			waitGroup.Add(1)
+			go func() {
+				defer waitGroup.Done()
+				io.Copy(os.Stdout, in)
+			}()
+		}
+		if in, err := llcmd.StderrPipe(); err != nil {
+			return err
+		} else {
+			waitGroup.Add(1)
+			go func() {
+				defer waitGroup.Done()
+				io.Copy(os.Stderr, in)
+			}()
+		}
+		if err := llcmd.Start(); err != nil {
+			return err
+		}
+		if err := LLVMCodeGen(ast, out); err != nil {
+			return err
+		}
+		out.Close()
+		if err := llcmd.Wait(); err != nil {
+			return err
+		}
+		waitGroup.Wait()
+		return nil
+	}
 	var llfile *os.File
 	if _llfile, err := ioutil.TempFile("", "blo"); err != nil {
 		return err
@@ -28,7 +70,6 @@ func compile(tokens <-chan Token) error {
 		llfile = _llfile
 	}
 	defer os.Remove(llfile.Name())
-	println("file=" + llfile.Name())
 	if err := LLVMCodeGen(ast, llfile); err != nil {
 		return err
 	}
@@ -38,7 +79,12 @@ func compile(tokens <-chan Token) error {
 		return err
 	}
 	defer os.Remove(llfile.Name() + ".o")
-	cccmd := exec.Command("gcc", llfile.Name()+".o")
+	var ccargs []string
+	if outflag == "-o" {
+		ccargs = []string{"-o", outfile}
+	}
+	ccargs = append(ccargs, llfile.Name()+".o")
+	cccmd := exec.Command("gcc", ccargs...)
 	if err := cccmd.Run(); err != nil {
 		return err
 	}
