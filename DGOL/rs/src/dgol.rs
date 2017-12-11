c0c658c67074a092a759215635aafc1e8c662285
Q.P.Liu <qpliu@yahoo.com>
2017-12-10 23:51:07 -0800

    Continue implementing DGOL in Rust.

--- DGOL/rs/src/dgol.rs
+++ DGOL/rs/src/dgol.rs
@@ -3,15 +3,19 @@ mod scope;
 
 fn main() {
     let mut node_pool = nodes::NodePool::new();
-    node_pool.gc_start();
-    for node in node_pool.new_node().iter() {
-        node_pool.gc_mark(node);
-        node.add_edge(node);
-        if node.has_edge(node) {
-            node.remove_edge(node);
-        }
-        let _ = node.edges();
+    let scope = scope::Scope::new(1,1,1);
+    let mut new_scope = scope.push(1,1,1,1);
+    let node = new_scope.new_node(&mut node_pool);
+    node.add_edge(&node);
+    if node.has_edge(&node) {
+        node.remove_edge(&node);
     }
-    node_pool.gc_sweep();
-    let mut scope = scope::Scope::new(1,1,1);
+    node.edges();
+    new_scope.clear_call_args();
+    new_scope.set_call_arg(0,1);
+    new_scope.set_var(0, &node);
+    new_scope.get_var(0, &mut node_pool);
+    new_scope.do_edges_start(0, &node);
+    new_scope.do_edges_next(0);
+    new_scope.do_edges_end(0);
 }
--- DGOL/rs/src/scope.rs
+++ DGOL/rs/src/scope.rs
@@ -1,16 +1,12 @@
-use std;
+use std::ptr;
+use super::nodes::{Node,NodePool};
 
 pub struct Scope<'a> {
-    scope_header: ScopeHeader<'a>,
-    params: Box<[*mut Option<super::nodes::Node>]>,
-    vars: Box<[Option<super::nodes::Node>]>,
-    do_edges: Box<[Option<Vec<super::nodes::Node>>]>,
-    call_args: Box<[*mut Option<super::nodes::Node>]>,
-}
-
-enum ScopeHeader<'a> {
-    Root(super::nodes::NodePool),
-    Parent(&'a mut Scope<'a>),
+    parent: Option<&'a Scope<'a>>,
+    params: Box<[*mut Option<Node>]>,
+    vars: Box<[Option<Node>]>,
+    do_edges: Box<[Option<Vec<Node>>]>,
+    call_args: Box<[*mut Option<Node>]>,
 }
 
 impl<'a> Scope<'a> {
@@ -20,9 +16,9 @@ impl<'a> Scope<'a> {
         let mut do_edges = Vec::with_capacity(do_edges_count);
         do_edges.resize(do_edges_count, None);
         let mut call_args = Vec::with_capacity(max_call_arg_count);
-        call_args.resize(max_call_arg_count, std::ptr::null_mut());
+        call_args.resize(max_call_arg_count, ptr::null_mut());
         Scope{
-            scope_header: ScopeHeader::Root(super::nodes::NodePool::new()),
+            parent: None,
             params: vec![].into_boxed_slice(),
             vars: vars.into_boxed_slice(),
             do_edges: do_edges.into_boxed_slice(),
@@ -30,23 +26,24 @@ impl<'a> Scope<'a> {
         }
     }
 
-    pub fn push(&'a mut self, param_count: usize, var_count: usize, do_edges_count: usize, max_call_arg_count: usize, args: &[*mut Option<super::nodes::Node>]) -> Self {
+    pub fn push(&'a self, param_count: usize, var_count: usize, do_edges_count: usize, max_call_arg_count: usize) -> Self {
+        assert!(var_count >= param_count);
         let mut vars = Vec::with_capacity(var_count);
         vars.resize(var_count, None);
         let mut do_edges = Vec::with_capacity(do_edges_count);
         do_edges.resize(do_edges_count, None);
         let mut params = Vec::with_capacity(param_count);
         for i in 0 .. param_count {
-            if i < args.len() {
-                params.push(args[i]);
+            if i < self.call_args.len() && !self.call_args[i].is_null() {
+                params.push(self.call_args[i]);
             } else {
-                params.push(&mut vars[i] as *mut Option<super::nodes::Node>);
+                params.push(&mut vars[i] as *mut Option<Node>);
             }
         }
         let mut call_args = Vec::with_capacity(max_call_arg_count);
-        call_args.resize(max_call_arg_count, std::ptr::null_mut());
+        call_args.resize(max_call_arg_count, ptr::null_mut());
         Scope{
-            scope_header: ScopeHeader::Parent(self),
+            parent: Some(self),
             params: params.into_boxed_slice(),
             vars: vars.into_boxed_slice(),
             do_edges: do_edges.into_boxed_slice(),
@@ -54,10 +51,104 @@ impl<'a> Scope<'a> {
         }
     }
 
-    fn node_pool(&mut self) -> &mut super::nodes::NodePool {
-        match self.scope_header {
-            ScopeHeader::Root(ref mut node_pool) => node_pool,
-            ScopeHeader::Parent(ref mut parent) => parent.node_pool(),
+    pub fn new_node(&self, node_pool: &mut NodePool) -> Node {
+        loop {
+            match node_pool.new_node() {
+                Some(node) => return node,
+                None => (),
+            }
+            node_pool.gc_start();
+            self.gc_mark(node_pool);
+            node_pool.gc_sweep();
         }
+    }
+
+    fn gc_mark(&self, node_pool: &mut NodePool) {
+        for option_var in self.vars.iter() {
+            for var in option_var.iter() {
+                node_pool.gc_mark(var);
+            }
+        }
+        for option_edges in self.do_edges.iter() {
+            for edges in option_edges.iter() {
+                for edge in edges.iter() {
+                    node_pool.gc_mark(edge);
+                }
+            }
+        }
+        match self.parent {
+            None => (),
+            Some(ref parent) => parent.gc_mark(node_pool),
+        }
+    }
+
+    pub fn clear_call_args(&mut self) {
+        for i in 0 .. self.call_args.len() {
+            self.call_args[i] = ptr::null_mut();
+        }
+    }
+
+    pub fn set_call_arg(&mut self, call_arg_index: usize, var_index: usize) {
+        assert!(call_arg_index < self.call_args.len());
+        assert!(var_index < self.vars.len());
+        if var_index < self.params.len() {
+            self.call_args[call_arg_index] = self.params[var_index];
+        } else {
+            self.call_args[call_arg_index] = &mut self.vars[var_index] as *mut Option<Node>;
+        }
+    }
+
+    pub fn get_var(&mut self, var_index: usize, mut node_pool: &mut NodePool) -> Node {
+        assert!(var_index < self.vars.len());
+        if var_index < self.params.len() {
+            unsafe {
+                match *self.params[var_index] {
+                    Some(ref node) => node.clone(),
+                    None => {
+                        let node = self.new_node(&mut node_pool);
+                        *self.params[var_index] = Some(node.clone());
+                        node
+                    },
+                }
+            }
+        } else {
+            match self.vars[var_index] {
+                Some(ref node) => node.clone(),
+                None => {
+                    let node = self.new_node(&mut node_pool);
+                    self.vars[var_index] = Some(node.clone());
+                    node
+                }
+            }
+        }
+    }
+
+    pub fn set_var(&mut self, var_index: usize, node: &Node) {
+        assert!(var_index < self.vars.len());
+        if var_index < self.params.len() {
+            unsafe {
+                *self.params[var_index] = Some(node.clone());
+            }
+        } else {
+            self.vars[var_index] = Some(node.clone());
+        }
+    }
+
+    pub fn do_edges_start(&mut self, do_edges_index: usize, node: &Node) {
+        assert!(do_edges_index < self.do_edges.len());
+        self.do_edges[do_edges_index] = Some(node.edges());
+    }
+
+    pub fn do_edges_next(&mut self, do_edges_index: usize) -> Option<Node> {
+        assert!(do_edges_index < self.do_edges.len());
+        for do_edges in self.do_edges[do_edges_index].iter_mut() {
+            return do_edges.pop();
+        }
+        panic!()
+    }
+
+    pub fn do_edges_end(&mut self, do_edges_index: usize) {
+        assert!(do_edges_index < self.do_edges.len());
+        self.do_edges[do_edges_index] = None;
     }
 }
