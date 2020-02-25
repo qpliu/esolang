53645c6ad72111d3c30bd4fe8f27c600b3ded5d2
Q.P.Liu <qpliu@yahoo.com>
2020-02-25 12:29:49 -0800

    Define and implement the Pointless programming language.

---
+++ pointless/README.md
@@ -0,0 +1,104 @@
+Pointless is a point-free variation on 01_.
+
+A Pointless program is a Pointless expression, which is a function that
+takes a list of bits and returns a list of bits.
+
+Syntax
+======
+
+Grammar
+-------
+```ebnf
+program = expr, "." ;
+
+expr = compose | let | if | concat | identifier ;
+
+compose = expr, expr ;
+
+let = "LET", binding, [ "," binding ], "IN" expr ;
+
+if = "(", expr, ",", expr, ",", expr, ")" ;
+
+concat = "(", expr, ",", expr, ")" ;
+
+binding = identifier, "=", expr ;
+```
+Identifiers are consecutive sequences of non-whitespace characters, excluding
+"(", ")", ",", "=", and ".".  Also, "LET" and "IN" are reserved and may not
+be used as identifiers.  Whitespace separates identifiers and is otherwise
+ignored.
+
+Comments begin with "==" and extend to the end of the line.
+
+Expressions
+===========
+
+Compose
+-------
+```
+f g
+```
+This is a function that returns the result of applying f to the result of
+appying g to its argument.
+
+Let
+---
+```
+LET f = (_,0 f,1 f) IN f
+```
+A LET expression binds one or more identifiers, which may be referred to
+in the body of the expression or in the bodies of the bindings.
+
+If
+--
+```
+(f,g,h)
+```
+If the argument of this function is an empty list, it returns the result
+of applying f to an empty list.
+
+If the argument of this function begins with a 0, it returns the result
+of applying g to the rest of argument that follows the 0.
+
+If the argument of this function begins with a 1, it returns the result
+of applying h to the rest of argument that follows the 1.
+
+Concat
+------
+```
+(f,g)
+```
+This is a function that returns the concatenation of the result applying f
+to its argument and the result of applying g to its argument.
+
+Functions
+---------
+Functions may be identifiers bound in an enclosing LET expression or a
+built-in function.  The built-in functions are as follows.
+
+### _
+_ ignores its argument and returns an empty list.
+
+### 0
+0 returns its argument with 0 prepended.
+
+### 1
+1 returns its argument with 1 prepended.
+
+Examples
+========
+
+Hello world
+-----------
+```
+0 0 0 1 0 0 1 0  1 0 1 0 0 1 1 0  0 0 1 1 0 1 1 0  0 0 1 1 0 1 1 0
+1 1 1 1 0 1 1 0  0 0 0 0 0 1 0 0  1 1 1 0 1 1 1 0  1 1 1 1 0 1 1 0
+0 1 0 0 1 1 1 0  0 0 1 1 0 1 1 0  0 0 1 0 0 1 1 0  1 0 0 0 0 1 0 0
+0 1 0 1 0 0 0 0 _.
+```
+
+Cat
+---
+```
+LET cat = (_,0 cat,1 cat) IN cat.
+```
---
+++ pointless/pointless.hs
@@ -0,0 +1,139 @@
+-- Build: ghc --make pointless
+-- Usage: ./pointless SRC-FILE
+
+import Data.Char(chr,isSpace,ord)
+import System.Environment(getArgs)
+import System.IO(char8,hPutStrLn,hSetEncoding,stderr,stdin,stdout)
+
+tokenize :: String -> [String]
+tokenize "" = []
+tokenize ('=':'=':rest) = tokenize (dropWhile (/= '\n') rest)
+tokenize (c:rest)
+  | isSpace c = tokenize rest
+  | c `elem` "().,=" = [c] : tokenize rest
+  | otherwise = tokenizeIdent [c] rest
+  where
+    tokenizeIdent ident "" = [reverse ident]
+    tokenizeIdent ident (c:rest)
+      | isSpace c || c `elem` "().,=" = reverse ident : tokenize (c:rest)
+      | otherwise = tokenizeIdent (c:ident) rest
+
+parse :: [String] -> Either String Expr
+parse tokens = do
+  (tokens,expr) <- parseExpr tokens
+  case tokens of
+    [] -> fail "Unexpected EOF"
+    (".":_) -> return expr
+    (token:_) -> fail ("Unexpected token: "++token)
+
+parseExpr :: [String] -> Either String ([String],Expr)
+parseExpr [] = fail "Unexpected EOF"
+parseExpr ("(":tokens) = parseGrouping [] tokens
+parseExpr ("LET":tokens) = parseLet [] tokens
+parseExpr (token:tokens)
+  | token `elem` [",",".","IN",")","="] = fail ("Unexpected token: "++token)
+  | otherwise = parseCompose (Func token) tokens
+
+parseCompose :: Expr -> [String] -> Either String ([String],Expr)
+parseCompose expr [] = fail "Unexpected EOF"
+parseCompose expr tokens@(token:_)
+  | token `elem` [",",")","IN","."] = return (tokens,expr)
+  | otherwise = do
+      (newTokens,newExpr) <- parseExpr tokens
+      return (newTokens,Compose expr newExpr)
+
+parseGrouping :: [Expr] -> [String] -> Either String ([String],Expr)
+parseGrouping _ [] = fail "Unexpected EOF"
+parseGrouping exprs tokens = do
+    (newTokens,newExpr) <- parseExpr tokens
+    case (length exprs,newTokens) of
+      (_,[]) -> fail "Unexpected EOF"
+      (n,",":newNewTokens) ->
+          if n < 2 then parseGrouping (newExpr:exprs) newNewTokens
+                   else fail "Unexpected token: ,"
+      (1,")":newNewTokens) -> parseCompose (Concat (head exprs) newExpr) newNewTokens
+      (2,")":newNewTokens) -> parseCompose (If (head (tail exprs)) (head exprs) newExpr) newNewTokens
+      (_,")":_) -> fail "Unexpected token: )"
+      (_,token:_) -> fail ("Unexpected token: "++token)
+
+parseLet :: [(String,Expr)] -> [String] -> Either String ([String],Expr)
+parseLet bindings [] = fail "Unexpected EOF"
+parseLet bindings (ident:"=":tokens)
+  | ident `elem` [".",",","=","(",")","LET","IN"] = fail ("Unexpected token: "++ident)
+  | otherwise = do
+      (newTokens,expr) <- parseExpr tokens
+      case newTokens of
+        [] -> fail "Unexpected EOF"
+        ("IN":newNewTokens) -> do
+          (newNewNewTokens,newExpr) <- parseExpr newNewTokens
+          return (newNewNewTokens,Let (reverse ((ident,expr):bindings)) newExpr)
+        (",":newNewTokens) -> parseLet ((ident,expr):bindings) newNewTokens
+        (token:_) -> fail ("Unexpected token: "++token)
+
+data Expr =
+    Compose Expr Expr
+  | Let [(String,Expr)] Expr
+  | If Expr Expr Expr
+  | Concat Expr Expr
+  | Func String
+
+initialScope :: String -> Maybe ([Bool] -> Either String [Bool])
+initialScope "_" = Just (const (return []))
+initialScope "0" = Just (return . (False:))
+initialScope "1" = Just (return . (True:))
+initialScope _ = Nothing
+
+resolve :: (String -> Maybe ([Bool] -> Either String [Bool])) -> Expr -> Either String ([Bool] -> Either String [Bool])
+resolve scope (Compose expr1 expr2) = do
+  f1 <- resolve scope expr1
+  f2 <- resolve scope expr2
+  return ((>>= f1) . f2)
+resolve scope (Let bindings expr) = resolve newScope expr
+  where
+    newScope ident = maybe (scope ident) Just
+                           (lookup ident (map (fmap bind) bindings))
+    bind expr = either (const . fail) id (resolve newScope expr)
+resolve scope (If expr1 expr2 expr3) = do
+  f1 <- resolve scope expr1
+  f2 <- resolve scope expr2
+  f3 <- resolve scope expr3
+  return (\ arg -> if null arg then f1 arg else if head arg then f3 (tail arg) else f2 (tail arg))
+resolve scope (Concat expr1 expr2) = do
+  f1 <- resolve scope expr1
+  f2 <- resolve scope expr2
+  return (\ arg -> do
+    v1 <- f1 arg
+    v2 <- f2 arg
+    return (v1 ++ v2))
+resolve scope (Func func) = do
+  maybe (fail ("Unbound identifier: "++func)) return (scope func)
+
+interp :: Expr -> [Bool] -> Either String [Bool]
+interp expr input = do
+  fn <- resolve initialScope expr
+  fn input
+
+fromBits :: [Bool] -> String
+fromBits bits
+  | null bits = []
+  | otherwise = chr (sum (zipWith bitVal (take 8 bits) [0..7])) : fromBits (drop 8 bits)
+  where
+    bitVal bit n = if bit then product (take n (repeat 2)) else 0
+
+toBits :: String -> [Bool]
+toBits = concatMap (byteBits 8 . ord)
+  where
+    byteBits b n | b <= 0 = []
+                 | otherwise = (mod n 2 == 1) : byteBits (b-1) (div n 2)
+
+run :: Expr -> IO ()
+run expr = do
+  hSetEncoding stdin char8
+  hSetEncoding stdout char8
+  interact (either id fromBits . interp expr . toBits)
+
+main :: IO ()
+main = do
+  (srcFile:_) <- getArgs
+  src <- readFile srcFile
+  either (hPutStrLn stderr) run (parse (tokenize src))
